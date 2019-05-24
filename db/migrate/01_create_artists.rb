419a27e0f558d2bc16ec4c70d2c3bda611654ed5
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-24 10:22:36 -0400

    Completed lab

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
